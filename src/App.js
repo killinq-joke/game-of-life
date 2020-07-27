bffc4e46601a435cb82d8e2e48baf9401a152f62
zakaria touzri <zakadmaster@gmail.com>
2020-07-27 19:29:23 +0200

    ADDED: array grid

--- src/App.js
+++ src/App.js
@@ -8,12 +8,13 @@ const App = () => {
   const [grid, setGrid] = useState(() => {
     const rows = [];
     for (let i = 0; i < numRows; i++) {
-      rows.push(Array(  ))
+      rows.push(Array.from(Array(numCols), () => 0))
     }
     return rows
   });
-  console.log(grid)
-  return <div></div>;
+  return <div>
+    
+  </div>;
 }
 
 export default App;
