20b3bbc68b98032c2f1eb53833bf9f624ac6deb6
zakaria touzri <zakadmaster@gmail.com>
2020-07-31 10:06:39 +0200

    ADDED: rules to Rules.js

--- src/Rules.js
+++ src/Rules.js
@@ -3,7 +3,10 @@ import { Link } from "react-router-dom";
 
 export default function Rules() {
   return <>
-  <p>rules</p>
+  <p>Any live cell with fewer than two live neighbours dies, as if by underpopulation.</p>
+  <p>Any live cell with two or three live neighbours lives on to the next generation.</p>
+  <p>Any live cell with more than three live neighbours dies, as if by overpopulation.</p>
+  <p>Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.</p>
   <Link to="/">PLAY</Link>
   </>;
 }
