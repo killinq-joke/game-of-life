33b7d9c115dcbdc5e42c9100e70661846ea205e2
zakaria touzri <zakadmaster@gmail.com>
2020-07-31 10:11:28 +0200

    ADDED: ol tag for rules

--- src/Rules.js
+++ src/Rules.js
@@ -2,11 +2,29 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function Rules() {
-  return <>
-  <p>Any live cell with fewer than two live neighbours dies, as if by underpopulation.</p>
-  <p>Any live cell with two or three live neighbours lives on to the next generation.</p>
-  <p>Any live cell with more than three live neighbours dies, as if by overpopulation.</p>
-  <p>Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.</p>
-  <Link to="/">PLAY</Link>
-  </>;
+  return (
+    <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+      <Link to="/">PLAY</Link>
+      <h3>Here are the rules of THE GAME OF LIFE</h3>
+      <ol>
+         <li>
+          Any live cell with fewer than two live neighbours dies, as if by
+          underpopulation.
+        </li>
+        <li>
+          Any live cell with two or three live neighbours lives on to the next
+          generation.
+        </li>
+        <li>
+          Any live cell with more than three live neighbours dies, as if by
+          overpopulation.
+        </li>
+        <li>
+          Any dead cell with exactly three live neighbours becomes a live cell,
+          as if by reproduction.
+        </li> 
+      </ol>
+        
+    </div>
+  );
 }
